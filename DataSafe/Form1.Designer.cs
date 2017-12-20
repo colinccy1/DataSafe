23400fd936a46b05d16c2d9227eca140633d8795
colinccy1 <47369555@qq.com>
2017-12-20 09:16:51 +0800

    20171219

--- DataSafe/Form1.Designer.cs
+++ DataSafe/Form1.Designer.cs
@@ -28,12 +28,34 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.test = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // test
+            // 
+            this.test.Location = new System.Drawing.Point(12, 12);
+            this.test.Name = "test";
+            this.test.Size = new System.Drawing.Size(104, 36);
+            this.test.TabIndex = 0;
+            this.test.Text = "test";
+            this.test.UseVisualStyleBackColor = true;
+            this.test.Click += new System.EventHandler(this.test_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(290, 65);
+            this.Controls.Add(this.test);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button test;
     }
 }
 
--- DataSafe/Form1.cs
+++ DataSafe/Form1.cs
@@ -5,8 +5,18 @@ using System.Data;
 using System.Drawing;
 using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Collections;
+using System.Text;
 using System.Windows.Forms;
+using System.Net;
+using Tool161;
+using System.Reflection;
+using System.IO;
 
 namespace DataSafe
 {
@@ -15,6 +25,39 @@ namespace DataSafe
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void test_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string path=@"C:\Windows\WindowColin.txt";
+                if(File.Exists(path))
+                {
+                    string text = File.ReadAllText(path);
+                    int num = Convert.ToInt32(text);
+                    if(num<5)
+                    {
+                        num++;
+                        File.WriteAllText(path, Convert.ToString(num));
+                    }
+                    else
+                    {
+                        MessageBox.Show("(:");
+                        Application.Exit();
+                    }
+                }
+                else
+                {
+                    string num = "1";
+                    File.WriteAllText(path, num);
+                }
+               
+            }
+            catch
+            {
+
+            }
         }
     }
 }
